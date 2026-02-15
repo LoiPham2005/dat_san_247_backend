92462f51c144caecfa7f70cc32c216dc5ae40a1a
LoiPham2005 <phamducloi919@gmail.com>
2026-02-15 17:46:03 +0700

    feat: Add modular Prisma schema definitions for AI, Chat, Payment, and other core domain models.

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,8 +5,8 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  // schema: "prisma/schema",
+  // schema: "prisma/schema.prisma",
+  schema: "prisma/schema",
   migrations: {
     path: "prisma/migrations",
   },
